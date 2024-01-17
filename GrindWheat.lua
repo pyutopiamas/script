728d3e05782b915e46495ddf35d789532d45295e
pyutopiamas <152804561+pyutopiamas@users.noreply.github.com>
2024-01-17 13:51:40 +0700

    Update GrindWheat.lua

--- GrindWheat.lua
+++ GrindWheat.lua
@@ -1,6 +1,11 @@
+--Do not change.
 worldWarp = 1
 runningSince = os.date("%H:%M")
-
+listenTo = "OnDialogRequest"
+findString = "It takes 50 per grind"
+canGrind = false
+worldSort = 1
+ 
 function wh()
   countFlour = 0
   for _, obj in ipairs(getObjects()) do
@@ -15,34 +20,50 @@ function wh()
   wbh.embed1.title = "Flour Tracker"
   wbh.embed1:addField("<:flour:1194082718791782421> "..countFlour.."x seed dropped.",true)
   wbh.embed1.footer.text = "Running "..#getBots().." bot(s) since "..runningSince.."."
-  wbh:send()
+  if webhookMsgId == nil and webhookMsgId == "" then
+    wbh:send()
+  end
+  if webhookMsgId ~= nil and webhookMsgId ~= "" then
+    wbh:edit(webhookMsgId)
+  end
 end
 wh()
-
-function checkTile()
-  local countObj = 0 print("Counting obj in tile.")
-  for _, obj in pairs(getObjects()) do
-    if obj.x // 32 == dropX + 1 and obj.y // 32 == dropY then
-      countObj = countObj + obj.count
-    end
-  end
-  print("Count = "..countObj)
-  if countObj < maxObjperTile then
-    return true
-  else
-    return false
-  end
-end
-
+ 
 while true do
   --Auto warp storage
   if getBot():getInventory():getItemCount(wheatId) < 50 and getBot():getInventory():getItemCount(flourId) < dropLimit then print("On condition auto warp")
+::rewarpTake::
     while not getBot():isInWorld(worlds[worldWarp]) or getBot():getWorld():getTile(getBot().x, getBot().y).fg == 6 do
       getBot():warp(worlds[worldWarp], worldId) print("warping")
       sleep(12000)
     end
+    count = 0
     for _, obj in ipairs(getObjects()) do
-      if obj.id == wheatId and getBot():getInventory():getItemCount(wheatId) < 50 then
+      if obj.id == wheatId and (#getBot():getPath(obj.x // 32, obj.y // 32) > 0 or getBot():isInTile(obj.x // 32, obj.y // 32)) do
+        count = count + obj.count
+      end
+      if count == 0 then
+        worldWarp = worldWarp + 1
+          worldWarp = worldWarp + 1 print("["..storageFlour[worldSort].."Storage has reached max capacity, moving to the next storage.")
+          if worldWarp > #worlds then
+            if loopMode == true then
+              worldWarp = 1 print("[LOOPMODE/ON] Reached the last input storage, looping to the first storage.")
+              goto rewarpTake
+            end
+            if loopMode == false then
+              worldWarp = 1 print("[LOOPMODE/OFF] Reached the last storage, stopping the script.")
+              getBot():warp("EXIT")
+              sleep(150)
+              if disconnectOnMax == true then
+                getBot():disconnect()
+              end
+              break
+            end
+          end
+        end
+ 
+    for _, obj in ipairs(getObjects()) do
+      if obj.id == wheatId and getBot():getInventory():getItemCount(wheatId) < 50 and (#getBot():getPath(obj.x // 32, obj.y // 32) > 0 or getBot():isInTile(obj.x // 32, obj.y // 32)) then
         if not getBot():isInTile(obj.x // 32, obj.y // 32) then
           getBot():findPath((obj.x // 32), (obj.y // 32)) print("Finding path to object")
           sleep(150)
@@ -72,17 +93,49 @@ while true do
       getBot():findPath(grindX, (grindY - 1)) print("Finding path to tile.")
       sleep(3000)
     end
-	sleep(1000)
+      sleep(1000)
     if getBot():isInTile(grindX, (grindY - 1)) and getBot():getInventory():getItemCount(wheatId) > 49 and getBot():getInventory():getItemCount(flourId) < 190 then
-      getBot():place(grindX, grindY, wheatId) print("Placing "..getInfo(wheatId).name.." at X: "..grindX.." Y: "..grindY)
-      sleep(300)
-      getBot():sendPacket(2, "action|dialog_return\ndialog_name|grinder\ntilex|"..tostring(grindX).."|\ntiley|"..tostring(grindY).."|\nitemID|"..tostring(wheatId).."|\ncount|"..tostring(getBot():getInventory():getItemCount(wheatId) // 50).."") print("Grinding on X:"..grindX.." Y: "..grindY)
+      grinding = false
+      function grind(varlist,netid)
+        print("Listening to packet, started.")
+        if varlist:get(0):getString() == listenTo then print("Listening to packet...")
+          if varlist:get(1):getString():find(findString) then print("Packet found, trying to match dialogue.")
+            canGrind = true print("Dialogue found, setting canGrind to true.")
+            unlistenEvents()
+          end
+        end
+      end
+      addEvent(Event.variantlist,grind)
+      while canGrind == false and getBot():getInventory():getItemCount(wheatId) >= 50 and getBot():getInventory():getItemCount(flourId) < 190 do
+        runThread(function()
+          for _, tile in pairs(getTiles()) do
+            if tile.fg == 4582 then
+              phX = tile.x
+              phY = tile.y
+              if getBot():isInTile(phX, phY - 1) or #getBot():getPath(tile.x, (tile.y - 1)) > 0 then
+                grindXa = tile.x print("Setting grinder tile.")
+                grindYa = tile.y
+              end
+            end
+          end
+          getBot():place(grindXa, grindYa, 880)
+          print("Placing "..getInfo(880).name.." at X: "..grindXa.." Y: "..grindYa)
+        end)
+        listenEvents(4) print("Start listening to event...")
+      end
+      if canGrind == true then
+        canGrind = false print("canGrind is true, setting it back to false.")
+        getBot():sendPacket(2, "action|dialog_return\ndialog_name|grinder\ntilex|"..tostring(grindX).."|\ntiley|"..tostring(grindY).."|\nitemID|"..tostring(wheatId).."|\ncount|"..tostring(getBot():getInventory():getItemCount(wheatId) // 50).."")
+        sleep(delayAfterGrind)
+      end
     end
   end
   --Auto drop flour
+  ::rewarpDrop::
   if getBot():getInventory():getItemCount(flourId) >= dropLimit then   print("In auto drop.")
-    while not getBot():isInWorld(storageFlour) do
-      getBot():warp(storageFlour, flStrgId) print("Warping.")
+    while not getBot():isInWorld(storageFlour[worldSort]) do
+      print(storageFlour[worldSort].." Checking warp condition.")
+      getBot():warp(storageFlour[worldSort], flourId) print("Warping.")
       sleep(12000)
     end
     setTile = 0
@@ -100,24 +153,28 @@ while true do
       end
     setTile = 1
     end
-    for _, tile in pairs(getTiles()) do
-      if tile.fg == 20 then
-        if tile:getExtra().label == "drop" then
-          initialDropX = tile.x
-          initialDropY = tile.y
-          dropX = initialDropX print("X: "..dropX)
-          dropY = initialDropY print("Y: "..dropY)
-          addDropX = 0
+    count = 0
+    for _, obj in pairs(getObjects()) do
+      if obj.id == flourId then
+        count = count + obj.count
+        if count >= maxFlourPerStorage then
+          worldSort = worldSort + 1 print("["..storageFlour[worldSort].."Storage has reached max capacity, moving to the next storage.")
+          if worldSort > #storageFlour then
+            if loopMode == true then
+              worldSort = 1 print("[LOOPMODE/ON] Reached the last storage, looping to the first storage.")
+              goto rewarpDrop
+            end
+            if loopMode == false then
+              worldSort = 1 print("[LOOPMODE/OFF] Reached the last storage, disconnecting the bot and stopping the script.")
+              getBot():warp("EXIT")
+              sleep(150)
+              if disconnectOnMax == true then
+                getBot():disconnect()
+              end
+              break
+            end
+          end
         end
-      end
-    end
-    while not checkTile() do
-      dropX = dropX + 1 print("Tile is full, moving to next tile. New X: ")
-      addDropX = addDropX + 1
-      if addDropX >= maxHorizontal then
-        dropY = dropY + 1 print("New Y: "..dropY)
-        dropX = initialDropX
-        addDropX = 0
       end
     end
     while not getBot():isInTile(dropX, dropY) do
@@ -137,7 +194,7 @@ while true do
         getBot():drop(flourId, getBot():getInventory():getItemCount(flourId)) print("Drop failed, attempting another try. Attempt: "..attempt)
         if attempt >= 5 then
           if addDropX < maxHorizontal then
-            dropX = dropX + 1 print("Assume that tile is full, moving to next tile. New X: ")
+            dropX = dropX + 1 print("Assume that tile is full, moving to next tile. New X: "..dropX)
             addDropX = addDropX + 1
           end
           if addDropX >= maxHorizontal then
@@ -148,8 +205,7 @@ while true do
         end
       end
     end
-  wh()
   end
 print("doing nothing.")
 sleep(300)
-end
+		end
